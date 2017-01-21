e9cdf3c8218102669d3d65ac278be0307ddbaf2c
objectsforheads <dev@objectsforheads.com>
2017-01-20 23:21:15 -0800

    sets up the scenebuilder board

--- imports/ui/pages/scene/build.js
+++ imports/ui/pages/scene/build.js
@@ -1,2 +1,11 @@
 import './build.html';
 import './build.scss';
+
+Template.scenebuilderBuild.helpers({
+  boardRows() {
+    return ['A', 'B', 'C', 'D', 'E'];
+  },
+  boardColumns() {
+    return [1,2,3,4,5,6,7,8,9];
+  }
+})
